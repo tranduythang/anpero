ac909f618f72d977722915d2642ae740a0c1b985
dong.pham <thangtd.hn@gmail.com>
2017-06-09 17:56:42 +0700

    add product category view

--- AnperoFrontend/Controllers/ProductController.cs
+++ AnperoFrontend/Controllers/ProductController.cs
@@ -12,26 +12,19 @@ namespace AnperoFrontend.Controllers
         [BuildCommonHtml]
         public ActionResult Index(int id)
         {
-
             WebService.AnperoService sv = new WebService.AnperoService();
             WebService.ProductItem item = sv.GetProductDetai(StoreID, TokenKey, id);
             WebService.SearchResult relateProduct = sv.SearchProduct(StoreID, TokenKey, item.CatID.ToString(), "", "", 0, 999999, 1, 5, "", SearchOrder.TimeDesc, 0);
-            WebService.ProductItem[] saleProduct;
-            if (HttpRuntime.Cache["saleProduct"] != null)
-            {
-                saleProduct = (WebService.ProductItem[])HttpRuntime.Cache["saleProduct"];
-            }
-            else
-            {
-                saleProduct = sv.GetSaleProduct(StoreID, TokenKey);
-                HttpRuntime.Cache.Insert("saleProduct", saleProduct, null, DateTime.Now.AddMinutes(shortCacheTime), TimeSpan.Zero);
-            }
-        
             ViewData["relateProduct"] = relateProduct;
             ViewData["prDetail"] = item;
-            ViewData["saleProduct"] = saleProduct;
             ViewBag.Title = item.PrName;
-
+            SetupCommonProduct();
+            return View();
+        }
+        [BuildCommonHtml]
+        public ActionResult Category(int id)
+        { 
+            SetupCommonProduct();
             return View();
         }
     }
