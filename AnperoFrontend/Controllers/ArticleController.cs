de38fa51cf0b6fa47465ad5a2238623d89540887
dong.pham <thangtd.hn@gmail.com>
2017-06-16 21:09:52 +0700

    Update blog

---
+++ AnperoFrontend/Controllers/ArticleController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AnperoFrontend.WebService;
+namespace AnperoFrontend.Controllers
+{
+    public class ArticleController : BaseController
+    {
+        // GET: Article
+        [BuildCommonHtml]
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        [BuildCommonHtml]
+        public ActionResult Category(int id)
+        {
+            int page = 1;
+            if (Request.QueryString["page"] != null)
+            {
+                page = Convert.ToInt32(Request.QueryString["page"]);
+            }
+            AnperoService sv = new AnperoService();
+            SearchArticleResults s = new SearchArticleResults();
+            s=sv.SearchArticle(StoreID, TokenKey, id, page, 12, 0);
+            ViewData["ArticleList"] = s;
+            ViewBag.page = Anpero.Paging.setUpPagedV2(page, 14, s.ResultsCount, 11, "?page=");
+            return View();
+        }
+    }
+}
--- AnperoFrontend/Controllers/BaseController.cs
+++ AnperoFrontend/Controllers/BaseController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using AnperoFrontend.WebService;
 namespace AnperoFrontend.Controllers
 {
     public class BaseController : Controller
