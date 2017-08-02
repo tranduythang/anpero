ba14474449b3fba4c9bc8a3b43c278391c2de83f
dong.pham <thangtd.hn@gmail.com>
2017-08-02 18:10:27 +0700

    update slide and images for product category

--- Shop_03_multiProduct/App_Start/BundleConfig.cs
+++ Shop_03_multiProduct/App_Start/BundleConfig.cs
@@ -8,7 +8,12 @@ namespace AnperoFrontend
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                       "~/Scripts/jquery-1-10.js",
+                       "~/Scripts/jquery.cokie.min.js",
+                       "~/Scripts/ShoppingCart.js",
+                       "~/Scripts/Anpero.Common.js",
+                       "~/assets/js/bootstrap.min.js"                       
                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
