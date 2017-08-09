1dfa54e65fc1b82c789862506fad86a4fc66861e
manh.lt <manh.lt@citigo.com.vn>
2017-08-09 16:24:11 +0700

    public demo

--- Shop_03_multiProduct/obj/Release/Package/PackageTmp/Scripts/ShoppingCart.js
+++ Shop_03_multiProduct/obj/Release/Package/PackageTmp/Scripts/ShoppingCart.js
@@ -21,8 +21,8 @@
             if (!checkExited) {
                 Cart.list.push({ id: _id, quantity: 1, price: _price, thumb: _thumb, title: _title });
             }
-        }        
-        $.cookie("CartList", JSON.stringify(Cart.list), {path: '/' });
+        }
+        $.cookie("CartList", JSON.stringify(Cart.list), { path: '/' });
         window.location.href = "/product/checkout";
     },
     addProduct2: function (_id, _price) {
@@ -40,29 +40,33 @@
         $.cookie("CartList", JSON.stringify(Cart.list), { path: '/' });
         Cart.bindCartTable();
     },
-    bindCart: function () {
+    bindCart: function () {        
         var ttSC = 0;
         var htmlCat = "";
         if ($.cookie("CartList") != 'null' && $.cookie("CartList") != "undefined" && $.cookie("CartList") != undefined) {
             Cart.list = jQuery.parseJSON($.cookie("CartList"));
             $(".spN").html(Cart.list.length);
             for (var i = 0; i < Cart.list.length; i++) {
-                ttSC += parseInt(Cart.list[i].price) * parseInt(Cart.list[i].quantity);
-                htmlCat += '<li class="product-info"><div class="p-left">';
-                htmlCat += '<a href="javascript:Cart.remove(' + Cart.list[i].id + ')" class="remove_link"></a>';
-                htmlCat += '<img class="img-responsive" src="' + Cart.list[i].thumb + '" alt="' + Cart.list[i].title + '">';
+                ttSC += parseInt(Cart.list[i].price) * parseInt(Cart.list[i].quantity);                
+                htmlCat += '<li class="item last"><div class="item-inner">'; 
+                htmlCat += '<a class="product-image" href="' + Cart.list[i].thumb + '"><img src="' + Cart.list[i].thumb + '"></a>';
+                htmlCat += '<div class="product-details">';
+                htmlCat += '<div class="access"><a class="btn-remove1" href="javascript:Cart.remove(' + Cart.list[i].id + ')">Xóa</a> </div>';
+                htmlCat += '<strong>' + Cart.list[i].quantity + '</strong> x <span class="price">' + Util.toMoneyFormat(Cart.list[i].price) + '</span>';                
+                htmlCat += '<p class="product-name">' + Cart.list[i].title + '</p>';
                 htmlCat += '</div>';
-                htmlCat += '<div class="p-right">';
-                htmlCat += '<p class="p-name">' + Cart.list[i].title + '</p>';
-                htmlCat += '<p class="p-rice">' + Util.toMoneyFormat(Cart.list[i].price) + ' đ</p>';
-                htmlCat += '<p>SL: ' + Cart.list[i].quantity + '</p>';
                 htmlCat += '</div>';
                 htmlCat += '</li>';
             }
-            $("#ttSC").html(Util.toMoneyFormat(ttSC));
-            $("#prCatCt").html(htmlCat);
+          
+            $("#cart-sidebar").html(htmlCat);           
             $(".toal-cart").show();
+            $(".mini-products-list").show();
             $(".cart-buttons").show();
+            $(".fl-mini-cart-content").show();
+            $("#cart-sidebar2").html(htmlCat);
+            $("#lpr").html(Util.toMoneyFormat(ttSC) + " đ");
+            
         }
     },
     bindCartTable: function () {
@@ -73,39 +77,43 @@
         var htmlCat = "";
         if ($.cookie("CartList") != null && $.cookie("CartList") != "undefined") {
             Cart.list = jQuery.parseJSON($.cookie("CartList"));
-           
-            $(".spN").html(Cart.list.length);
-            for (var i = 0; i < Cart.list.length; i++) {
-                ttSC += parseInt(Cart.list[i].price) * parseInt(Cart.list[i].quantity);
-                htmlCat += '<tr>';
-                htmlCat += '<td class="cart_product">';
-                htmlCat += '<a href="#"><img src="' + Cart.list[i].thumb + '" alt="' + Cart.list[i].title + '"></a>';
-                htmlCat += '</td>';
-                htmlCat += '<td class="cart_description">';
-                htmlCat += '<p class="product-name"><a href="#">' + Cart.list[i].title + ' </a></p>';
+            try {
+                $(".spN").html(Cart.list.length);
+                for (var i = 0; i < Cart.list.length; i++) {
+                    ttSC += parseInt(Cart.list[i].price) * parseInt(Cart.list[i].quantity);
+                    htmlCat += '<tr>';
+                    htmlCat += '<td class="cart_product">';
+                    htmlCat += '<a href="#"><img src="' + Cart.list[i].thumb + '" alt="' + Cart.list[i].title + '"></a>';
+                    htmlCat += '</td>';
+                    htmlCat += '<td class="cart_description">';
+                    htmlCat += '<p class="product-name"><a href="#">' + Cart.list[i].title + ' </a></p>';
 
-                htmlCat += '</td>';
-                htmlCat += '<td class="cart_avail"><span class="label label-success">Sẵn sàng</span></td>';
-                htmlCat += '<td class="price"><span>' + Util.toMoneyFormat(Cart.list[i].price) + '</span></td>';
-                htmlCat += '<td class="qty">';
-                htmlCat += '<input class="form-control input-sm" type="text" value="' + Cart.list[i].quantity + '" id="prQuantity_' + Cart.list[i].id + '">';
-                htmlCat += '<a href="javascript:Cart.addProduct2(' + Cart.list[i].id + ',' + Cart.list[i].price + ');"><i class="fa fa-caret-up"></i></a>';
-                htmlCat += '<a href="javascript:Cart.remove3(' + Cart.list[i].id + ',' + Cart.list[i].price + ');"><i class="fa fa-caret-down"></i></a>';
-                htmlCat += '</td>';
-                htmlCat += '<td class="price">';
-                htmlCat += '<span>' + Util.toMoneyFormat(parseInt(Cart.list[i].price) * parseInt(Cart.list[i].quantity)) + ' đ</span>';
-                htmlCat += '</td>';
-                htmlCat += '<td class="action">';
-                htmlCat += '<a href="javascript:Cart.remove2(' + Cart.list[i].id + ')" class="remove_link"></a>';
-                htmlCat += '</td>';
-                htmlCat += '</tr>';
+                    htmlCat += '</td>';
+                    htmlCat += '<td class="cart_avail"><span class="label label-success">Sẵn sàng</span></td>';
+                    htmlCat += '<td class="price"><span>' + Util.toMoneyFormat(Cart.list[i].price) + '</span></td>';
+                    htmlCat += '<td class="qty">';
+                    htmlCat += '<input class="form-control input-sm" type="text" value="' + Cart.list[i].quantity + '" id="prQuantity_' + Cart.list[i].id + '">';
+                    htmlCat += '<a href="javascript:Cart.addProduct2(' + Cart.list[i].id + ',' + Cart.list[i].price + ');"><i class="fa fa-caret-up"></i></a>';
+                    htmlCat += '<a href="javascript:Cart.remove3(' + Cart.list[i].id + ',' + Cart.list[i].price + ');"><i class="fa fa-caret-down"></i></a>';
+                    htmlCat += '</td>';
+                    htmlCat += '<td class="price">';
+                    htmlCat += '<span>' + Util.toMoneyFormat(parseInt(Cart.list[i].price) * parseInt(Cart.list[i].quantity)) + ' đ</span>';
+                    htmlCat += '</td>';
+                    htmlCat += '<td class="action">';
+                    htmlCat += '<a href="javascript:Cart.remove2(' + Cart.list[i].id + ')" class="remove_link"></a>';
+                    htmlCat += '</td>';
+                    htmlCat += '</tr>';
 
+                }
+                var shipingFee = $('input[name=radio_3]:checked').attr("data-ship");
+
+                $("#ttPrCt").html(Util.toMoneyFormat(ttSC) + " đ");
+                $("#ttOdCt").html(Util.toMoneyFormat(parseInt(ttSC) + parseInt(shipingFee) + parseInt(_paymentFee)) + " đ");
+                $("#prCatCtTable").html(htmlCat);
+            } catch (e) {
+                $(".spN").html("0");
             }
-            var shipingFee = $('input[name=radio_3]:checked').attr("data-ship");
 
-            $("#ttPrCt").html(Util.toMoneyFormat(ttSC) + " đ");
-            $("#ttOdCt").html(Util.toMoneyFormat(parseInt(ttSC) + parseInt(shipingFee) + parseInt(_paymentFee)) + " đ");
-            $("#prCatCtTable").html(htmlCat);
         }
     },
     remove: function (prId) {
@@ -127,7 +135,7 @@
             if (Cart.list[i].id == prId) {
                 Cart.list.splice(i, 1);
             }
-        }        
+        }
         $.cookie("CartList", JSON.stringify(Cart.list), { path: '/' });
         Cart.bindCartTable();
     },
@@ -141,7 +149,7 @@
                 Cart.list[i].price = parseInt(Cart.list[i].price);
                 Cart.list[i].quantity = parseInt(Cart.list[i].quantity) - 1;
             }
-        }        
+        }
         $.cookie("CartList", JSON.stringify(Cart.list), { path: '/' });
         Cart.bindCartTable();
     },
@@ -152,7 +160,7 @@
         var _shipingType = $('input[name=radio_3]:checked').val();
         var _paymentType = $('input[name=radio_4]:checked').val();
         var _shipingFee = $('input[name=radio_3]:checked').attr("data-ship");
-        var _paymentFee = $('input[name=radio_4]:checked').attr("data-ship"); 
+        var _paymentFee = $('input[name=radio_4]:checked').attr("data-ship");
         var _email = $("#cMail").val();
         var _phone = $("#cPhone").val();
         var _address = $("#cAddress").val();
@@ -187,25 +195,25 @@
             Util.notify("Lỗi", "Giỏ hàng đang trống không thể tạo đơn hàng");
             valid = false;
         }
-        _payMentType=0;
+        _payMentType = 0;
         switch (_shipingType) {
             case "0":
-                    _payMentType = 0;
+                _payMentType = 0;
                 break;
-             case "1":
-                 switch (_paymentType) {
-                     case "0":
-                         _payMentType = 0;
-                         break;
-                     case "1":
-                         _payMentType = 6;
-                         break;
-                     case "2":
-                         _payMentType = 1;
-                         break;
-                     default:
-                 }
-                 break;
+            case "1":
+                switch (_paymentType) {
+                    case "0":
+                        _payMentType = 0;
+                        break;
+                    case "1":
+                        _payMentType = 6;
+                        break;
+                    case "2":
+                        _payMentType = 1;
+                        break;
+                    default:
+                }
+                break;
             case "2":
                 switch (_paymentType) {
                     case "0":
@@ -223,7 +231,7 @@
 
             default:
                 _payMentType = 0;
-        }        
+        }
         //    case 3:
         //        return "Thanh toán online và chuyển phát thường";
         //    case 4:
@@ -237,14 +245,14 @@
                 method: "post",
                 url: "/handler/ProductHandler.ashx",
                 datatype: "text/plain",
-                data: { op: "CreateOrder", detail: _detail,PayMentType:_payMentType, captcha: captchaResponse, name: _name, email: _email, phone: _phone, address: _address, ProductList: $.cookie("CartList"), shipingFee:parseInt(_shipingFee)+parseInt(_paymentFee) },
+                data: { op: "CreateOrder", detail: _detail, PayMentType: _payMentType, captcha: captchaResponse, name: _name, email: _email, phone: _phone, address: _address, ProductList: $.cookie("CartList"), shipingFee: parseInt(_shipingFee) + parseInt(_paymentFee) },
                 success: function (rs) {
                     $("#ajax_loader").hide();
                     if (!isNaN(rs)) {
                         $.removeCookie('CartList', { path: '/' });
                         $("#cartContent2").html("<h4>Đơn hàng số #" + rs + " đã được gửi thành công tới bộ phận bán hàng. Cảm ơn bạn đã sử dụng dịch vụ của chúng tôi!</h4>");
                         Util.notify("", "Đơn hàng đã được gửi tới bộ phận bán hàng. ");
-                     
+
                     } else {
                         Util.notify("", rs);
                         $("#cartContent1").show();
