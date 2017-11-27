cad7e827fd95e80a4d112a2f7865becf40bce0d7
thangtd-home <thangtd.hn@gmail.com>
2017-11-28 01:09:57 +0700

    add theme 10

--- Anpero/StringHelpper.cs
+++ Anpero/StringHelpper.cs
@@ -313,6 +313,61 @@ namespace Anpero
         {
             return ConVertToMoneyFormatInt(input.ToString());
         }
+       public static string FormatPhoneNumber(string phoneNumber)
+        {
+
+            if (String.IsNullOrEmpty(phoneNumber))
+                return phoneNumber;
+
+            Regex phoneParser = null;
+            string format = "";
+
+            switch (phoneNumber.Length)
+            {
+
+                case 5:
+                    phoneParser = new Regex(@"(\d{3})(\d{2})");
+                    format = "$1 $2";
+                    break;
+
+                case 6:
+                    phoneParser = new Regex(@"(\d{2})(\d{2})(\d{2})");
+                    format = "$1 $2 $3";
+                    break;
+
+                case 7:
+                    phoneParser = new Regex(@"(\d{3})(\d{2})(\d{2})");
+                    format = "$1 $2 $3";
+                    break;
+
+                case 8:
+                    phoneParser = new Regex(@"(\d{4})(\d{2})(\d{2})");
+                    format = "$1 $2 $3";
+                    break;
+
+                case 9:
+                    phoneParser = new Regex(@"(\d{4})(\d{3})(\d{2})(\d{2})");
+                    format = "$1 $2 $3 $4";
+                    break;
+
+                case 10:
+                    phoneParser = new Regex(@"(\d{3})(\d{3})(\d{2})(\d{2})");
+                    format = "$1 $2 $3 $4";
+                    break;
+
+                case 11:
+                    phoneParser = new Regex(@"(\d{4})(\d{3})(\d{2})(\d{2})");
+                    format = "$1 $2 $3 $4";
+                    break;
+
+                default:
+                    return phoneNumber;
+
+            }//switch
+
+            return phoneParser.Replace(phoneNumber, format);
+
+        }
         public static String ConVertToMoneyFormat(String s)
         {
             int dotIndex = s.LastIndexOf(".");
