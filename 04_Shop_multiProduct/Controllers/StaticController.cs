d33e3ae6cc670083947ead8b585a43fec9cfacd0
thangtd-home <thangtd.hn@gmail.com>
2018-06-19 02:38:37 +0700

    z
    z

--- 04_Shop_multiProduct/Controllers/StaticController.cs
+++ 04_Shop_multiProduct/Controllers/StaticController.cs
@@ -10,13 +10,16 @@ namespace AnperoFrontend.Controllers
     {
         // GET: Static
         [OutputCache(Duration = 300, VaryByParam = "none")]
-        public String Css()
+        public filele Css()
         {
             Response.AppendHeader("Cache-Control", "max-age=1200,stale-while-revalidate=3600"); // HTTP 1.1.
             Response.AppendHeader("Pragma", "no-cache"); // HTTP 1.0.
-            Response.AppendHeader("Cache-Control", "public");            
+            Response.AppendHeader("Cache-Control", "public");
+            Response.AppendHeader("Content-Type", "text/css");
+            
             WebService.AnperoService service = new WebService.AnperoService();
             return service.GetWebContent(StoreID, TokenKey, 7);
+           // return File("", contentType, "Report.doc");
 
         }
     }
