f513dd8d8fb9ca29c7dd4b903d1faa4809a057a1
Tran Thang <thangtd.hn@gmail.com>
2019-04-06 16:18:19 +0700

    no message

--- Anpero/UrlHelper.cs
+++ Anpero/UrlHelper.cs
@@ -8,12 +8,19 @@ namespace Anpero
 {
     class UrlHelper
     {
+        /// <summary>
+        /// return root name of web example http://anpero.com
+        /// </summary>
+        /// <returns>return root name of web example http://anpero.com</returns>
+        public static string GetRootLink()
+        {
+            return System.Web.HttpContext.Current.Request.Url.Scheme + @"://" + System.Web.HttpContext.Current.Request.Url.Host;
+        }
         public static string GetRootUrlReferrer()
         {
             try
             {
-                Uri myReferrer = System.Web.HttpContext.Current.Request.UrlReferrer;
-                //  Uri myReferrer= System.Web.HttpContext.Current.Request.Url;
+                Uri myReferrer = System.Web.HttpContext.Current.Request.UrlReferrer;                
                 string actual = myReferrer.PathAndQuery.ToString();
                 String[] spitActual = actual.Split('/');
                 return spitActual[0] + @"//" + spitActual[2];
