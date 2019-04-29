4230622dfc0f295e70addf2e395eba8bf9f677d8
ChuKhaLi <chungdinhnguyen.ukb@gmail.com>
2019-04-29 13:12:08 +0700

    no message

---
+++ 12_Shop_MultiProduct/Helper/UrlExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+public static class UrlExtensions
+{
+    public static string Content(this UrlHelper urlHelper, string contentPath, bool toAbsolute = false)
+    {
+        var path = urlHelper.Content(contentPath);
+        var url = new Uri(HttpContext.Current.Request.Url, path);
+
+        return toAbsolute ? url.AbsoluteUri : path;
+    }
+}
