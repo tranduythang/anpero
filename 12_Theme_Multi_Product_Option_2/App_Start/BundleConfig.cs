683ea664cfdf4e0b4f2e49ada68b738b5f376c53
ChuKhaLi <chungdinhnguyen.ukb@gmail.com>
2019-05-15 00:07:46 +0700

    no message

---
+++ 12_Theme_Multi_Product_Option_2/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Optimization;
+
+namespace _12_Theme_Multi_Product_Option_2
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundle/js").Include(
+                "~/Content/theme/js/jquery-1.10.2.min.js",
+                 "~/Scripts/jquery.cokie.min.js",
+                 "~/Content/theme/js/bootstrap.min.js",
+                 "~/Content/theme/js/owl.carousel.min.js",
+                 "~/Content/theme/js/javascript.js",
+                 "~/Scripts/Anpero.Common.js",
+                 "~/Scripts/ShoppingCart.js"
+             ));
+
+            bundles.Add(new StyleBundle("~/bundle/css").Include(
+                "~/Content/theme/css/bootstrap.min.css",
+                "~/Content/theme/css/font-awesome.min.css",
+                "~/Content/theme/css/style.css"
+            ));
+        }
+    }
+}
