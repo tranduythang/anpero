43d10d466d68ebbc6064adb01b378b9bcf776af6
Anpero <thangtd.hn@gmail.com>
2019-05-26 05:26:43 +0700

    UPDATE THEME 12 V2

--- 12_Theme_Multi_Product_Option_2/Scripts/Anpero.Common.js
+++ 12_Theme_Multi_Product_Option_2/Scripts/Anpero.Common.js
@@ -10,28 +10,28 @@
             title: '<strong>' + _title + '</strong>',
             message: '<strong>' + _mesage + '</strong>'
         }, {
-            type: 'warning',
-            delay: 3000,
-            z_index: 9999,
-            placement: {
-                from: "top",
-                align: "center"
-            }
+                type: 'warning',
+                delay: 3000,
+                z_index: 9999,
+                placement: {
+                    from: "top",
+                    align: "center"
+                }
 
-        });
+            });
 
     },
     isUrl: function (string) {
         var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
-  '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-  '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-  '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+            '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+            '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
         return pattern.test(string);
     }
 
-}
+};
 
 $(window).scroll(function () {
    
