531e2aaaf8932418cdd0f756bd95fa95bd4460d9
Anpero <thangtd.hn@gmail.com>
2019-07-31 03:21:53 +0700

    no message

---
+++ 14_8_CompanyInfo/Controllers/ArticleController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AnperoFrontend.WebService;
+namespace AnperoFrontend.Controllers
+{
+    public class ArticleController : BaseController
+    {
+        // GET: Article
+        [BuildCommonHtml]
+        public ActionResult Index(int id)
+        {
+            SetUpCommonArticle();
+            AnperoService ws = new AnperoService();
+            BlogItem rs= ws.GetArticleById(StoreID, TokenKey, id);
+            ViewBag.Title = rs.Title;
+            ViewBag.WebsiteUrl = Request.Url.Scheme + System.Uri.SchemeDelimiter + Request.Url.Host + Anpero.StringHelpper.GetArticleLink(rs.Title, rs.Id);
+            ViewData["blogdeltail"] = rs;
+            SearchArticleResults s = new SearchArticleResults();
+            s = ws.SearchArticle(StoreID, TokenKey, rs.CategoryId, 1, 5, 0);
+            ViewData["ArticleList"] = s;
+            return View();
+        }
+        [BuildCommonHtml]
+        public ActionResult Category(int id)
+        {
+            int page = 1;
+            if (Request.QueryString["page"] != null)
+            {
+                page = Convert.ToInt32(Request.QueryString["page"]);
+            }
+            AnperoService sv = new AnperoService();
+            SearchArticleResults s = new SearchArticleResults();
+            s=sv.SearchArticle(StoreID, TokenKey, id, page, 12, 0);
+            ViewData["ArticleList"] = s;
+            ViewBag.page = Anpero.Paging.setUpPagedV2(page, 14, s.ResultsCount, 11, "?page=");            
+            ViewBag.WebsiteUrl = Request.Url.Scheme + System.Uri.SchemeDelimiter + Request.Url.Host + "/blog";
+            if (id == 0)
+            {
+                ViewBag.CategoryName = "Blog";
+                ViewBag.Title = "Tin tức";
+            }
+            else
+            {
+                if (s.ItemList.Length > 0)
+                {
+                    ViewBag.Title = s.ItemList[0].CategoryName;
+                    ViewBag.CategoryName = s.ItemList[0].CategoryName;
+                }
+               
+            }
+            SetUpCommonArticle();
+            return View();
+        }
+        private void SetUpCommonArticle()
+        {
+
+            WebService.AnperoService service = new WebService.AnperoService();
+            WebService.Ads[] Slide = null;
+            if (HttpRuntime.Cache["categoryMenuList"] != null)
+            {
+                ViewData["categoryMenuList"] = (List<WebService.BlogCategory>)HttpRuntime.Cache["categoryMenuList"];
+            }
+            else
+            {
+                List<WebService.BlogCategory> categoryList = service.GetBlogCategory(StoreID, TokenKey).ToList();
+                ViewData["categoryMenuList"] = categoryList;
+                if (categoryList != null)
+                {
+                    HttpRuntime.Cache.Insert("categoryMenuList", categoryList, null, DateTime.Now.AddMinutes(shortCacheTime), TimeSpan.Zero);
+                }
+               
+            }
+            if (HttpRuntime.Cache["slide3"] != null)
+            {
+                ViewData["slide3"] = (WebService.Ads[])HttpRuntime.Cache["slide3"];
+            }
+            else
+            {
+                Slide = service.GetAdsSlide(StoreID, TokenKey, PageContent.Ads3);
+                ViewData["slide3"] = Slide;
+                if (Slide != null)
+                {
+                    HttpRuntime.Cache.Insert("slide3", Slide, null, DateTime.Now.AddMinutes(shortCacheTime + 6), TimeSpan.Zero);
+                }
+            }
+            GetTopArticle();
+        }
+    }
+}
---
+++ 14_8_CompanyInfo/Controllers/ProductController.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnperoFrontend.Controllers
+{
+    public class ProductController : BaseController
+    {
+
+        // GET: Product
+        [BuildCommonHtml]
+        public ActionResult Index(int id)
+        {
+            WebService.AnperoService sv = new WebService.AnperoService();
+            WebService.ProductItem item = sv.GetProductDetail(StoreID, TokenKey, id);
+            WebService.SearchResult relateProduct = sv.SearchProduct(StoreID, TokenKey, item.CatID.ToString(), "", "", 0, 999999, 1, 5, "", SearchOrder.TimeDesc, 0);
+            ViewData["relateProduct"] = relateProduct;
+            ViewData["prDetail"] = item;
+            ViewBag.Title = item.PrName;
+            SetupCommonProduct();
+            return View();
+        }
+        [BuildCommonHtml]
+        public ActionResult Category(int id)
+        {
+            string pageQuery = Request.QueryString["page"];
+            int page = 1;
+            if (!string.IsNullOrEmpty(pageQuery))
+            {
+                page = Convert.ToInt32(pageQuery);
+            }
+            WebService.AnperoService sv = new WebService.AnperoService();
+            WebService.SearchResult rs   = sv.SearchProduct(StoreID, TokenKey, id.ToString(), "", "", 0, 999999, page, 14, "", SearchOrder.TimeDesc, 0);
+            ViewData["productList"] = rs;
+            ViewBag.page =Anpero.Paging.setUpPagedV2(page, 14, rs.ResultCount, 10, "?page=");
+            
+            if (rs!=null && rs.Item.Length > 0)
+            {
+                ViewBag.Title = rs.Item[0].CatName;
+            }
+            
+            SetupCommonProduct();
+            SetUpSeo(2, id);
+            return View("List");
+        }
+        [BuildCommonHtml]
+        public ActionResult Group(int id)
+        {
+            string pageQuery = Request.QueryString["page"];
+            int page = 1;
+            if (!string.IsNullOrEmpty(pageQuery))
+            {
+                page = Convert.ToInt32(pageQuery);
+            }
+            WebService.AnperoService sv = new WebService.AnperoService();
+            WebService.SearchResult rs = sv.GetProductByGroup(StoreID, TokenKey, id, page, 14, 0);
+            ViewData["productList"] = rs;
+            ViewBag.page = Anpero.Paging.setUpPagedV2(page, 14, rs.ResultCount, 10, "?page=");
+
+            ViewBag.isParent = "1";
+            if (rs != null && rs.Item.Length > 0)
+            {
+                ViewBag.Title = rs.Item[0].ParentCatName;
+            }
+            SetupCommonProduct();
+            SetUpSeo(1, id);
+            return View("List");
+        }
+        [BuildCommonHtml]
+        public ActionResult ParentCategory(int id)
+        {
+            string pageQuery = Request.QueryString["page"];
+            int page = 1;
+            if (!string.IsNullOrEmpty(pageQuery))
+            {
+                page = Convert.ToInt32(pageQuery);
+            }
+            WebService.AnperoService sv = new WebService.AnperoService();
+            WebService.SearchResult rs = sv.GetProductByParentCategory(StoreID, TokenKey, id, page, 14, 0);
+            ViewData["productList"] = rs;
+            ViewBag.page = Anpero.Paging.setUpPagedV2(page, 14, rs.ResultCount, 10, "?page=");
+            
+            ViewBag.isParent = "1";
+            if (rs != null && rs.Item.Length > 0)
+            {
+                ViewBag.Title = rs.Item[0].ParentCatName;
+            }
+            SetupCommonProduct();
+            SetUpSeo(1,id);
+            return View("List");
+        }
+        [BuildCommonHtml]
+        public ActionResult Search(string category, string keyword)
+        {            
+            string pageQuery = Request.QueryString["page"];
+            int page = 1;
+            if (!string.IsNullOrEmpty(pageQuery))
+            {
+                page = Convert.ToInt32(pageQuery);
+            }
+            WebService.AnperoService sv = new WebService.AnperoService();
+            WebService.SearchResult rs = sv.SearchProduct(StoreID, TokenKey, category, "", "", 0, 999999999, page, 14, keyword, SearchOrder.NameDesc, 0);
+            ViewData["productList"] = rs;
+            ViewBag.page = Anpero.Paging.setUpPagedV2(page, 14, rs.ResultCount, 10, "?page=");            
+            if (rs != null && rs.Item.Length > 0)
+            {
+                ViewBag.Title = rs.Item[0].CatName;
+            }
+            SetupCommonProduct();
+            SetUpSeo(0,0);
+            return View("List");
+        }
+        [BuildCommonHtml]
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+         private void SetUpSeo(int type,int categoryId)
+        {
+
+            AnperoFrontend.WebService.Webconfig commonInfo=null;
+            if (HttpRuntime.Cache["commonInfo"] != null)
+            {
+                ViewData["commonInfo"] = HttpRuntime.Cache["commonInfo"];
+                commonInfo = (AnperoFrontend.WebService.Webconfig)HttpRuntime.Cache["commonInfo"];
+            }
+            else
+            {
+                WebService.AnperoService service = new WebService.AnperoService();
+                var rs = service.GetCommonConfig(CommonConfig.StoreID, CommonConfig.TokenKey);
+                ViewData["commonInfo"] = rs;
+                commonInfo = rs;
+                if (rs != null)
+                {
+                    HttpRuntime.Cache.Insert("commonInfo", rs, null, DateTime.Now.AddMinutes(shortCacheTime), TimeSpan.Zero);
+                }
+            }
+
+            //Get Description and Keywords of Category production
+            ViewBag.Description = string.Empty;
+            ViewBag.Keywords = string.Empty;
+            ViewBag.WebsiteUrl = string.Empty;
+            ViewBag.ImageUrl = string.Empty;
+            if (commonInfo != null)
+            {
+                switch (type)
+                {
+                    case 1:
+                        foreach (var item in commonInfo.ProductCategoryList)
+                        {
+                            if (item.Id == categoryId)
+                            {
+                                ViewBag.Keywords = item.Keywords;
+                                ViewBag.Description = item.Description;
+                                ViewBag.WebsiteUrl = Request.Url.Scheme + System.Uri.SchemeDelimiter + Request.Url.Host +
+                                 Anpero.StringHelpper.GetParentCategoryLink(item.Name, item.Id);
+                                ViewBag.ImageUrl = item.Images;
+                                break;
+                            }
+                        }
+                        break;
+                    case 2:
+                        foreach (var item in commonInfo.ProductCategoryList)
+                        {
+                            foreach (var chidItem in item.ChildCategory)
+                            {
+                                if (chidItem.Id == categoryId)
+                                {
+                                    ViewBag.Keywords = item.Keywords;
+                                    ViewBag.Description = item.Description;
+                                    ViewBag.WebsiteUrl = Request.Url.Scheme + System.Uri.SchemeDelimiter + Request.Url.Host +
+                                     Anpero.StringHelpper.GetCategoryLink(chidItem.Name, chidItem.Id);
+                                    ViewBag.ImageUrl = item.Images;
+                                    break;
+                                }
+                            }
+
+                        }
+                        break;
+                    default:
+                        ViewBag.Keywords = "Tìm kiếm " + commonInfo.Name + "| " + commonInfo.Desc;
+                        ViewBag.Description = "Tìm kiếm trên " + commonInfo.Name + "| " + commonInfo.Desc;
+                        ViewBag.WebsiteUrl = Request.Url.AbsoluteUri;
+                        ViewBag.ImageUrl = commonInfo.Logo;
+                        break;
+                }
+
+            }
+          
+        }
+    }
+}
