4746947db2a4f88c18451790c690dc9fd2f589ed
Tran Thang <thang.td@citigo.net>
2019-11-02 19:03:33 +0700

    no message

---
+++ Anpero/ConvertHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anpero
+{
+   public static class ConvertHelper
+    {
+        public static DateTime ConvertTodateTime(string inputString)
+        {
+            DateTime datetime = new DateTime();
+            try
+            {
+                datetime = DateTime.ParseExact(inputString, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None);
+            }
+            catch (Exception)
+            {
+
+                datetime = DateTime.ParseExact(inputString, "dd/MM/yyyy h:mm tt",  CultureInfo.InvariantCulture, DateTimeStyles.None);
+            }
+            return datetime;
+        }
+    }
+}
