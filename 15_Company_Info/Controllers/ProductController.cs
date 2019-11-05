d1fb57e222d190aee2ae87443a48fc69890070cb
anpero <thangtd.hn@gmail.com>
2019-11-06 02:17:58 +0700

    no message

--- 15_Company_Info/Controllers/ProductController.cs
+++ 15_Company_Info/Controllers/ProductController.cs
@@ -17,10 +17,9 @@ namespace AnperoFrontend.Controllers
             WebService.ProductItem item = sv.GetProductDetail(StoreID, TokenKey, id);
             WebService.SearchResult relateProduct = sv.SearchProduct(StoreID, TokenKey, item.CatID.ToString(), "", "", 0, 999999, 1, 5, "", SearchOrder.TimeDesc, 0, string.Empty);
             ViewData["relateProduct"] = relateProduct;
-            ViewData["prDetail"] = item;
             ViewBag.Title = item.PrName;
             SetupCommonProduct();
-            return View();
+            return View(item);
         }
         [BuildCommonHtml]
         public ActionResult Category(int id)
