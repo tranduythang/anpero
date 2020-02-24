c50b6c87e3db7b29b2e586a17fe796fd7271d7b8
anpero <thangtd.hn@gmail.com>
2020-02-25 01:28:44 +0700

    no message

--- 16_Shop_MultiProduct/assets/home-init.js
+++ 16_Shop_MultiProduct/assets/home-init.js
@@ -1,12 +1,14 @@
 var homeFontend = function () {
     function bindProduct(categoryId) {
-        
+       
         $("#feature-pr-content").addClass("loading-lazy");
+        $("#feature-pr-content").html("");
         $.ajax({
             url: "/product/getByCategory",
             type:"post",
             data: { id: categoryId },
             success: function (data) {
+                debugger
                 $("#feature-pr-content").removeClass("loading-lazy");
                 $("#feature-pr-content").html(data);
             }
